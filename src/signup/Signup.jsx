f1c325e2c85d7f7b43ba99dd05eb436760874b77
webpriya <priyachouhan1019@gmail.com>
2022-01-08 04:37:02 -0800

    [Priyanka] Added : signup page Left part

--- src/signup/Signup.jsx
+++ src/signup/Signup.jsx
@@ -17,11 +17,34 @@ export class Signup extends Component {
                            <p>Create your Google Account</p>
                        </div>
                        <div class="name-row">
-                           <div>  <TextField id="outlined-basic" label="First Name" variant="outlined" /></div>
-                           <div> <TextField id="outlined-basic" label="Last Name" variant="outlined" /></div>
+                           <div class="first-name">  <TextField id="outlined-basic" label="First Name" variant="outlined" /></div>
+                           <div class="last-name"> <TextField id="outlined-basic" label="Last Name" variant="outlined" /></div>
                        </div>
+                       <div class="user-row">
+                       <TextField id="outlined-basic" label="Your email address" variant="outlined" fullWidth helperText="You can use letters,numbers  & periods" />
+                       </div>
+                       <div class="use_current">
+                           <p>use the current email address instead</p>
+                        </div>
+                        <div class="password-row">
+                           <div class="password">  <TextField id="outlined-basic" label="Password" variant="outlined" /></div>
+                           <div class="c-password"> <TextField id="outlined-basic" label="Confirm Password" variant="outlined" /></div>
+                           </div>
+                           <div class="checkbox">
+                            <input class="check1" type="checkbox"></input>
+                            <p class="showbox">Show Password</p>
+                        </div>
+                        <div className="sign-instead">
+
+                            
+                                {/* <button className="button1">Next</button> */}
+
+                        </div>
+                       </div>
+                   
+                   <div class="right-part">
+
                    </div>
-                   <div class="right-part"></div>
                </div>
            </div>
         )
